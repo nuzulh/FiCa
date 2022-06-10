e16be98237d9103244251433861f11caab482da5
nuzulh <nuzulhikam0@gmail.com>
2022-06-10 21:24:04 +0700

    CC App Engine

---
+++ Cloud Computing/db.py
@@ -0,0 +1,59 @@
+import os
+import pymysql
+
+db_user = os.environ.get('CLOUD_SQL_USERNAME')
+db_password = os.environ.get('CLOUD_SQL_PASSWORD')
+db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
+db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
+
+
+def open_connection():
+    if os.environ.get('GAE_ENV') == 'standard':
+        unix_socket = '/cloudsql/{}'.format(db_connection_name)
+        cnx = pymysql.connect(user=db_user, password=db_password,
+                              unix_socket=unix_socket, db=db_name)
+        return cnx
+    else:
+        host = '127.0.0.1'
+        cnx = pymysql.connect(user='root', password='',
+                              host=host, db='fica')
+        return cnx
+
+
+def get():
+    conn = open_connection()
+    with conn.cursor() as cursor:
+        result = cursor.execute('SELECT * FROM fish;')
+        fishes = cursor.fetchall()
+        if result > 0:
+            return fishes
+        else:
+            return {'message': 'No fishes found'}
+
+
+def get_one(label):
+    conn = open_connection()
+    with conn.cursor() as cursor:
+        result = cursor.execute('SELECT * FROM fish WHERE label=%s', (label))
+        fish = cursor.fetchall()
+        if result > 0:
+            fish = fish[0]
+            res = {}
+            for _ in range(len(fish)):
+                res["label"] = fish[1]
+                res["name"] = fish[2]
+                res["description"] = fish[3]
+                res["min_price"] = fish[4]
+                res["max_price"] = fish[5]
+            return res
+        else:
+            return {'message': f'No fishes found by label {label}'}
+
+
+def create(fish):
+    conn = open_connection()
+    with conn.cursor() as cursor:
+        cursor.execute('INSERT INTO fishes (label, name, description, min_price, max_price) VALUES(%s, %s, %s, %s, %s)',
+                       (fish["label"], fish["name"], fish["description"], fish["min_price"], fish["max_price"]))
+    conn.commit()
+    conn.close()
