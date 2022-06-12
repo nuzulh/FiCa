4c6266d23f765fb288a9c1b11898d9392c4296f0
cell6969 <bossmarinoo@gmail.com>
2022-06-12 21:25:07 +0700

    update test folder

---
+++ MOBILE DEVELOPMENT/app/src/test/java/com/bangkit/fica/ExampleUnitTest.kt
@@ -0,0 +1,14 @@
+package com.bangkit.fica
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+
+    //////////////TESTING/////////////
+    @Test
+    fun test1() {
+        var varValue = "A"
+        assertEquals("", varValue)
+    }
+}
